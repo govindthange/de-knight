9f90bcd3b9a2d51d6f1249b5888a89ada4fd0b45
Govind Thange <gthange@yahoo.com>
2022-10-21 18:59:26 +0530

    Remove dummy-feature from the main screen #27

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-import {useSelector} from 'react-redux';
-import Login from './features/authentication/components/Login';
-import DummyFeature from './features/dummy-feature';
-import {hasLoggedIn} from './features/authentication/authenticationSlice';
 import Chessboard from './features/chessboard';
 
 function App() {
-  const isLoggedIn = useSelector(hasLoggedIn);
-
   return (
     <>
-      {!isLoggedIn && <Login />}
-      {isLoggedIn && <DummyFeature />}
       <Chessboard />
     </>
   );
