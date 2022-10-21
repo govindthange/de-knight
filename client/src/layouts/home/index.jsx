eca65584b1010d07ecacb7cc3a67cf9148f96d90
Govind Thange <gthange@yahoo.com>
2022-10-21 21:20:36 +0530

    Add flow to initiate game in home screen #30

--- client/src/layouts/home/index.jsx
+++ client/src/layouts/home/index.jsx
@@ -1,7 +1,84 @@
+import './index.css';
 import React from 'react';
+import {useState} from 'react';
+import * as uuid from 'uuid';
+import {useHistory} from 'react-router-dom/cjs/react-router-dom.min';
 
 function Home() {
-  return <div>Home</div>;
+  const [showModal, setShowModal] = useState(false);
+  const history = useHistory();
+
+  const newGameOptions = [
+    {label: 'Black pieces', value: 'b'},
+    {label: 'White pieces', value: 'w'},
+    {label: 'Random', value: 'r'}
+  ];
+
+  const onPlayLocally = () => {
+    alert(uuid.v4());
+  };
+
+  const onPlayOnline = () => {
+    setShowModal(true);
+  };
+
+  const startOnlineGame = startingPiece => {
+    const member = {
+      uid: 'some-uuid',
+      piece: startingPiece === 'r' ? ['b', 'w'][Math.round(Math.random())] : startingPiece,
+      name: localStorage.getItem('de-chess-user'),
+      creator: true
+    };
+
+    const game = {
+      status: 'waiting',
+      members: [member],
+      gameId: `${uuid.v4()}`
+    };
+
+    console.log(`Save following object to database:%o`, game);
+    alert(`Save following object to database:\n${JSON.stringify(game)}`);
+
+    history.push(`/play/${game.gameId}`);
+  };
+
+  return (
+    <>
+      <div className="columns home">
+        <div className="column has-background-primary home-columns">
+          <button className="button is-link" onClick={onPlayLocally}>
+            Play Locally
+          </button>
+        </div>
+        <div className="column has-background-link home-columns">
+          <button className="button is-primary" onClick={onPlayOnline}>
+            Play Online
+          </button>
+        </div>
+      </div>
+      <div className={`modal ${showModal ? 'is-active' : ''}`}>
+        <div className="modal-background"></div>
+        <div className="modal-content">
+          <div className="card">
+            <div className="card-content">
+              <div className="content">Please Select the piece you want to start</div>
+            </div>
+            <footer className="card-footer">
+              {newGameOptions.map(({label, value}) => (
+                <span
+                  className="card-footer-item pointer"
+                  key={value}
+                  onClick={() => startOnlineGame(value)}>
+                  {label}
+                </span>
+              ))}
+            </footer>
+          </div>
+        </div>
+        <button className="modal-close is-large" onClick={() => setShowModal(false)}></button>
+      </div>
+    </>
+  );
 }
 
 export default Home;
