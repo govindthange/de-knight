15b395b1bafa702151d835e8344a0b086df91fd1
Govind Thange <gthange@yahoo.com>
2022-10-26 12:57:36 +0530

    Implement play-event

--- client/src/features/chat/components/SocketIoDemo/index.jsx
+++ client/src/features/chat/components/SocketIoDemo/index.jsx
@@ -7,14 +7,23 @@ function SocketIoDemo() {
 
   const [room, setRoom] = useState(rooms[0]);
   const [transcript, setTranscript] = useState([]);
+  const [move, setMove] = useState('');
+  const [positions, setPositions] = useState([]);
 
   // Get memoized callbacks.
-  const onEvent = useCallback((e, obj) => {
-    console.log(`Received '${e}' event w/ ${JSON.stringify(obj)} data`);
-    setTranscript(oldTranscript => [obj, ...oldTranscript]);
-  }, []);
+  const listenerMap = {
+    chat: useCallback((e, obj) => {
+      console.log(`Received '${e}' event w/ ${JSON.stringify(obj)} data`);
+      setTranscript(oldTranscript => [obj, ...oldTranscript]);
+    }, []),
 
-  const {isConnected, emit} = useSocketIo('chat', onEvent);
+    play: useCallback((e, move) => {
+      console.log(`Received '${e}' event w/ ${JSON.stringify(move)} data`);
+      setPositions(oldPositions => [move, ...oldPositions]);
+    }, [])
+  };
+
+  const {isConnected, emit} = useSocketIo(listenerMap);
 
   useEffect(() => {
     console.count('SocketIoDemo::useEffect() calls: ');
@@ -25,10 +34,14 @@ function SocketIoDemo() {
     emit('chat', {room, message: text});
   }, []);
 
+  const playMove = useCallback(m => {
+    emit('play', {room, move: m});
+  }, []);
+
   return (
     <div>
       <h1>
-        SocketIoDemo Room: {room} | {isConnected ? 'connected' : 'disconnected'}
+        Play Room: {room} | {isConnected ? 'connected' : 'disconnected'}
       </h1>
 
       {rooms.map((r, i) => (
@@ -37,13 +50,22 @@ function SocketIoDemo() {
         </button>
       ))}
 
-      <h1>Live SocketIoDemo:</h1>
+      <h1>Chat:</h1>
 
       <input type="text" name="name" value={message} onChange={e => setMessage(e.target.value)} />
       <button onClick={() => sendMessage(message)}>Send</button>
 
       {transcript.map((m, i) => (
         <p key={i}>{m}</p>
+      ))}
+      <br />
+
+      <h1>Player Moves:</h1>
+
+      <input type="text" name="move" value={move} onChange={e => setMove(e.target.value)} />
+      <button onClick={() => playMove(move)}>Play</button>
+      {positions.map((d, i) => (
+        <p key={i}>{d}</p>
       ))}
     </div>
   );
