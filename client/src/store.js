6e2ca0a88495dd73c9b134d3ac3dfcdf6772c532
Govind Thange <gthange@yahoo.com>
2022-10-27 08:11:58 +0000

    Fix structure as per standard chess-game ref.

--- client/src/store.js
+++ client/src/store.js
@@ -1,19 +1,22 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from './features/authentication/authenticationSlice';
 import chessboardReducer from './features/chessboard/chessboardSlice';
+/*
 import modalReducer from './features/dummy-feature/components/ModalDialog/redux-slice';
 import dummyDataReducer from './features/dummy-feature/dummyDataSlice';
 import dummyThunkReducerForFetch from './features/dummy-feature/dummyThunkSliceForFetch';
 import dummyThunkReducerForAxios from './features/dummy-feature/dummyThunkSliceForAxios';
-
+*/
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    chessboard: chessboardReducer,
+    chessboard: chessboardReducer
+    /*
     modal: modalReducer,
     dummyAsyncData: dummyDataReducer,
     dummyAsyncDataFromFetch: dummyThunkReducerForFetch,
     dummyAsyncDataFromAxios: dummyThunkReducerForAxios
+    */
   }
 });
 
